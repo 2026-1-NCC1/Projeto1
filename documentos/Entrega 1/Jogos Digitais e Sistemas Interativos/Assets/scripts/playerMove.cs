e630acc0640814cc0b9116cd5961dc648fe42040
GabrielOliveira <gabriel.oliveira4@edu.fecap.br>
2026-03-31 11:49:02 -0300

    alteracoes
    alteracoes do script player life, tag de ammo, tag de alvo, caixa de municao criada

--- documentos/Entrega 1/Jogos Digitais e Sistemas Interativos/Assets/scripts/playerMove.cs
+++ documentos/Entrega 1/Jogos Digitais e Sistemas Interativos/Assets/scripts/playerMove.cs
@@ -1,52 +1,34 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class playerMove : MonoBehaviour
-
 {
-
-    public Rigidbody rb;
+    Rigidbody rb;
     public float moveSpeed = 8f;
     public float jumpForce = 8f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        
     }
-    void resetPosicao()
-    {
-        transform.position = new Vector3(-1.7f, 0.74f, 1f);
-    }
-    // Update is called once per frame
+
     void Update()
     {
-
         float horizontalInput = Input.GetAxis("Horizontal");
-
-        if (rb.linearVelocity.z <= 35)
-        {
-            rb.linearVelocity = new Vector3(horizontalInput * moveSpeed, rb.linearVelocity.y, 0.01f + rb.linearVelocity.z);
-        }
-
-
 
         if (Input.GetButtonDown("Jump") || Input.GetButton("Vertical"))
         {
             rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z);
         }
-        
+
+        if (rb.linearVelocity.z <= 30)
+        {
+            rb.linearVelocity = new Vector3(horizontalInput * moveSpeed, rb.linearVelocity.y, 0.01f + rb.linearVelocity.z);
+        }
+
     }
 
- 
-    private void OnTriggerEnter(Collider hit)
-    {
-        if ((hit.gameObject.tag == "obstaculos")|| hit.gameObject.tag == "limites" )
-        {
-            resetPosicao();
-        }
-    }
 }
 
 
