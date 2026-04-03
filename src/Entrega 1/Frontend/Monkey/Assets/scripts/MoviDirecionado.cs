43c1230a56af525e53cf0d2d1fc2db308251ccf3
Giovanna Bueno <giovannabueno@macMeu.local>
2026-04-03 14:49:23 -0300

    organizacao de pastas e codigo

---
+++ src/Entrega 1/Frontend/Monkey/Assets/scripts/MoviDirecionado.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class MoviDirecionado : MonoBehaviour
+{
+    //listar as variaveis tanto do script SapwnerControler quanto as que foram criadas aqui
+    private Vector3 direcao;
+    private float velocidade;
+    private float tempoVida;
+    private float timer;
+    private bool inicializado = false;
+
+    //fun��o para puxar as vari�veis do outro scritpt como tamb�m declarar o valor das criadas aqui
+    public void Inicializar(Vector3 dir, float vel, float vida)
+    {
+        direcao = dir;
+        velocidade = vel;
+        tempoVida = vida;
+        timer = 0f;
+        inicializado = true;
+    }
+    // start vazio novamente porque usar� apenas o trigger do click para funcionar
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        //quando iniciado ele come�a a contar a quano tempo o tiro esta em tela para destruir ele quando o tempo for maior que o timer
+        if (!inicializado) return;
+
+        transform.Translate(direcao * velocidade * Time.deltaTime, Space.World);
+
+        timer += Time.deltaTime;
+        if (timer >= tempoVida)
+        {
+            Destruir();
+        }
+
+        
+    }
+
+    //fun��o destruir o tiro depois de certo tempo
+    void Destruir()
+    {
+        Destroy(gameObject);
+    }
+
+    //Refazer para ele destruir o alvo
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("limites") || other.CompareTag("inimigos"))
+        {
+            Destruir();
+        }
+    }
+}
---
+++ src/Entrega 1/Frontend/Monkey/Assets/scripts/SpawnerControler.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class SpawnerControler : MonoBehaviour
+{
+    //organizar varia1veis que serão controladas pelo inspector
+    [Header("Configurações")]
+    public GameObject tiro;
+    public float veloMovimento = 5f;
+    public float tempoVida = 3f;
+    public GameObject player;
+   
+    //Start vazio porque o spawn tiro só vai ser chamado quando clicado
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        //pega o input do mouse click esquerdo para fazer o tiro spawnar e pega a localizaçao do player
+        if (Input.GetMouseButtonDown(0))
+        {
+            Spawnar();
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+    }
+    
+    // função para spawnar o tiro uma posição na frente do player e direcionar o tiro para a posição do click
+    void Spawnar()
+    {
+       //pega o raio da câmera e a posição do player +1 para o tiro sempre sair do player
+        Ray raio = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        Vector3 posicaoplayer = player.transform.position + new Vector3(0,0,1);
+
+        //quando o raio colidir com algo ele pega a posição do click ele atira e cria um novo objeto sapawnado que some no tempo determinado
+        if (Physics.Raycast(raio, out hit))
+        {
+            Vector3 posicaoClique = hit.point;
+            Vector3 posicaoSpawn = posicaoplayer;
+            GameObject novoObjeto = Instantiate(tiro, posicaoSpawn, Quaternion.identity);
+            Vector3 direcao = (posicaoClique - posicaoSpawn).normalized;
+
+            MoviDirecionado movimento = novoObjeto.AddComponent<MoviDirecionado>();
+            movimento.Inicializar(direcao, veloMovimento, tempoVida);
+        }
+    }
+}
