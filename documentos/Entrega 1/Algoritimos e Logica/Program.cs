a7d7ee11d8eaef8f85d4b4d99f0d114400814571
oliviaworks <oliviagithub9@gmail.com>
2026-04-09 15:57:08 -0300

    change

--- documentos/Entrega 1/Algoritimos e Logica/Program.cs
+++ documentos/Entrega 1/Algoritimos e Logica/Program.cs
@@ -2,59 +2,138 @@
 using System.Threading;
 
 public class Program
-
 {
-    static int pontos = 0;
-    static int energia = 100;
-
     public static void Main()
     {
-        Fase1();
-    }
+        Lore();
 
-    public static void Fase1()
-    {
-        // pergunta 1
-        Console.WriteLine("""
-            1 - Quem foi a primeira pessoa a viajar no Espaço?
+        int energia = 10;
+
+        while (energia > 0)
+        {
+            int pontos = 0;
+            int erros = 0;
+
+            if (Desitir())
+            {
+                break;
+            }
+
+            // Pergunta 1
+            Console.WriteLine("""
+
+            [QUESTÃO 01]
+            Quem foi a primeira pessoa a viajar no Espaço?
                 [a] Yuri Gagarin
                 [b] A cadela Laika
                 [c] Neil Armstrong
             Digite sua resposta:
-        """);
-        string resposta = Console.ReadLine().ToLower();
-        AtualizarPontuacaoEnergia("a", resposta);
+            """);
+            string resposta = Console.ReadLine().ToLower();
+            (pontos, erros) = AtualizarPontuacaoErros("a", resposta, pontos, erros);
+            if (Desitir())
+            {
+                break;
+            }
 
-        // pergunta 2
-        Console.WriteLine("""
-            2 - Quando tempo a luz do Sol demora para chegar na Terra?
-                [a] 1 minuto
-                [b] 5 minutos
-                [c] 8 minutos
+            // Pergunta 2
+            Console.WriteLine("""
+
+            [QUESTÃO 02]
+            Quais são os respectivos valores de X e Y do Sistema de Equações abaixo?
+                [a] 𝑥 = 4, 𝑦 = 5
+                [b] 𝑥 = 3, 𝑦 = 2/5
+                [c] 𝑥 = 10, 𝑦 = 10
             Digite sua resposta:
-        """);
-        resposta = Console.ReadLine().ToLower();
-        AtualizarPontuacaoEnergia("c", resposta);
+            """);
+            resposta = Console.ReadLine().ToLower();
+            (pontos, erros) = AtualizarPontuacaoErros("c", resposta, pontos, erros);
+            if (Desitir())
+            {
+                break;
+            }
+
+            // Pergunta 3
+            Console.WriteLine("""
+
+            [QUESTÃO 03]
+            A Luz do Sol demora quando tempo para chegar à Terra?
+                [a] 1 minuto
+                [b] 8 minutos
+                [c] 5 minutos
+            Digite sua resposta:
+            """);
+            resposta = Console.ReadLine().ToLower();
+            (pontos, erros) = AtualizarPontuacaoErros("b", resposta, pontos, erros);
+
+            Console.WriteLine($"Você acertou {pontos}/3");
+
+            if (pontos == 3)
+            {
+                Console.WriteLine("[Fantasma] Parabéns, você conseguiu...Pode sair");
+                break;
+            }
+
+            energia -= erros;
+
+            if (energia <= 0)
+            {
+                Console.WriteLine("Sua energia acabou...");
+                break;
+            }
+
+            Console.WriteLine($"""
+
+            Você perdeu {erros} de energia
+                Energia: {energia}
+            """);
+        }
     }
-    
-    public static void AtualizarPontuacaoEnergia(string respostaCerta, string respostaJogador)
+
+    public static (int, int) AtualizarPontuacaoErros(string respostaCerta, string respostaJogador, int pontos, int erros)
     {
         if (respostaJogador != respostaCerta)
         {
-            energia--;
+            erros++;
         }
         else
         {
             pontos++;
         }
 
-        MostrarPontuacaoEnergia();
+        return (pontos, erros);
     }
-    public static void MostrarPontuacaoEnergia()
+
+    public static bool Desitir()
     {
-        Console.WriteLine($"""
-            Pontuação: {pontos}
-            Energia: {energia}
-        """);
+        Console.WriteLine("\nDeseja desistir?\n[1] Sim\nPressione qualquer outra tecla para continuar");
+        string input = Console.ReadLine();
+
+        if (input == "1")
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public static void Lore()
+    {
+        Console.WriteLine("[Você] ZzzzZzzzz...");
+        Thread.Sleep(1000);
+        Console.WriteLine("[Rádio] EII, ACORDA!");
+        Thread.Sleep(1000);
+        Console.WriteLine("[Você] 👀");
+        Thread.Sleep(1000);
+        Console.Write("[Rádio] Parece que você dormiu no laboratório...Qual é o seu nome?\nDigite seu nome: ");
+        string nomeJogador = Console.ReadLine();
+        Console.WriteLine($"[Rádio] Certo, {nomeJogador}...Como você pode perceber já anoiteceu. Para o seu azar, esse laboratório é assombrado pelo fantasma de um professor que tinha muito apego por esse lugar há muuuuitos anos. Só ele pode deixar você sair daí..");
+        Thread.Sleep(7000);
+        Console.WriteLine($"[{nomeJogador}] E como que eu saio daqui?!");
+        Thread.Sleep(2000);
+        Console.WriteLine($"[Rádio] Você tem que fazer uma prova, mas cuidado... A cada questão que você erra, ele suga pontos da sua energia vital, até você ficar sem.. Se você acertar tudo, ele te deixa sair. Você pode tentar quantas vezes quiser, mas cuidado para não deixar ele sugar todos os pontos da sua energia.");
+        Thread.Sleep(10000);
+        Console.WriteLine($"[{nomeJogador}] E se eu não souber nada?!");
+        Thread.Sleep(2000);
+        Console.WriteLine($"[Rádio] Você pode desistir a qualquer momento da prova ou nem faze-la, mas você terá que esperar amanhecer o dia para alguém destrancar o laboratório. Bom, eu não recomendo desistir, você não gostaria de passar a noite com um fantasma te rodeando, mas enfim você quem sabe...");
     }
 }
